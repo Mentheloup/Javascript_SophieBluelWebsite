5258aede4bf2e744edfcd191b351a191dc3b411d
ChaberJo <cjf.humbert@gmail.com>
2025-09-23 16:53:30 +0200

    Generate filters buttons

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,5 +1,7 @@
 const serveur = "http://localhost:5678/api/";
 
+
+//RECUPERER INFO BACK-END
 async function fetchWorks() {
     const response = await fetch("http://localhost:5678/api/works");
     const works = await response.json();
@@ -8,28 +10,60 @@ async function fetchWorks() {
 
 const works = await fetchWorks();
 
-function genererWorks (works) {
-// Récupérer le futur parent
-const parentGalery = document.querySelector(".gallery");
+// BOUTONS FILTRES
+const listeFiltres = new Set();
 
-parentGalery.innerHTML += '';
+// On régupère les valeurs des filtres
+function genererListeFiltres (works) {
+
+    listeFiltres.add("Tous");
+
+    for (let i = 0; i < works.length; i++) {
+        listeFiltres.add(works[i].category.name);
+    }
+
+}
+
+// On génère les boutons
+function genererBoutonsFiltres (listeFiltres) {
+
+    //Sous quelle balise
+    const baliseFilter = document.querySelector("filter");
+
+    baliseFilter.innerHTML = '';
+    
+    //Pour chaque element du set, créer bouton
+    for (const element of listeFiltres) {
+        let nouveauFiltre = '<h2>' + element + '</h2>';
+        baliseFilter.innerHTML += nouveauFiltre;
+    }
+}
+
+// WORKS
+
+function genererWorks (works) {
+    // Récupérer le futur parent
+    const parentGalery = document.querySelector(".gallery");
+
+    parentGalery.innerHTML += '';
 
     // Boucle pour constituer le bout d'HTML pour chaque work
     for (let i = 0; i < works.length; i++) {
         const work = works[i];
 
-        console.log(work);
+        let newWork = 
+        '<figure>'
+        + '<img src="' + work.imageUrl +'" alt="'+ work.title +'">'
+        + '<figcaption>' + work.title + '</figcaption>'
+        + '</figure>'
+        ;
 
-    let newWork = 
-      '<figure>'
-    + '<img src="' + work.imageUrl +'" alt="'+ work.title +'">'
-    + '<figcaption>' + work.title + '</figcaption>'
-    + '</figure>'
-    ;
-
-    parentGalery.innerHTML += newWork;
-    // parentGalery.appendChild(newWork);
+        parentGalery.innerHTML += newWork;
     }
 }
 
+//UTILISER LES FONCTIONS
+
+genererListeFiltres (works);
+genererBoutonsFiltres (listeFiltres);
 genererWorks(works);
